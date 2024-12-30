d0a36e92afcbc7efbff20299487260109d4551cc
1nVisible <paskoarsen@gmail.com>
2024-12-30 11:54:37 +0200

    Implemented LSTM model on captchas dataset

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,14 +1,70 @@
-from netCDF4 import Dataset
-import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from torchvision.io import read_image
+from torchvision.transforms import ToTensor
+import torch
 
+import netCDF4
+import numpy as np
+import os
+
+__all__=[
+    "CaptchasDataSet", "DataLoader"
+]
+
+'''
 def load_temperature(filename) -> np.array:
-    file=Dataset(filename, "rs") # 's' for unbuffered
+    file=netCDF4.Dataset(filename, "rs") # 's' for unbuffered
     print(file.groups, file.dimensions)
     file.close()
     return None
+'''
+
+class ClimateDataset(Dataset):
+    def __init__(self, folder):
+        self.folder=folder
+        self.is_iterable=False
+    
+    def __iter__(self):
+        self.is_iterable=True
+        return self
+    
+    def __next__(self):
+        if not self.is_iterable:
+            raise TypeError(f"{type(self).__name__!r} object is not an iterator")
+        
+        return NotImplemented
+
+
+class CaptchasDataSet(Dataset):
+    def __init__(self, folder):
+        self.folder=folder
+        self.raw_image_labels=[
+            (read_image(os.path.join(self.folder, filename))[0],
+             filename.removesuffix(".jpeg").split("_")[-1])
+            for filename in os.listdir(self.folder)
+        ]
+        
+        flatten_func=torch.nn.Flatten(start_dim = 0)
+        self.data=[
+            (torch.stack(tuple(map(flatten_func, image.split(50, dim=1)))),
+             torch.Tensor(list(map(ord, label))))
+            for image, label in self.raw_image_labels
+        ]
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, index):
+        return self.data[index]
+
 
 def main():
-    load_temperature("data/data.nc")
+    #load_temperature("data/data.nc")
+    dataset=CaptchasDataSet("data/captchas/test")
+    print(f"{len(dataset)=}")
+    img300=dataset[300][0]
+    print(img300.shape, img300.split(50, dim=2)[1].shape)
+    return
 
 if __name__=="__main__":
     main()
--- src/glob_temp_nn.py
+++ src/glob_temp_nn.py
@@ -7,22 +7,94 @@ from torchvision import transforms
 from torchvision.datasets import ImageFolder
 #import timm # unneccesary shit
 
-class TemperatureModel(nn.Module):
-    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim, **kwargs):
-        super().__init__(**kwargs)
-        self.flatten = nn.Flatten() #
-        self.lstm = nn.LSTM()
+#import data_loader
+from data_loader import *
 
-    def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
-        return logits
+__all__=[
+    "TemperatureModel", "main"
+]
+
+class TemperatureModel(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, output_size,
+                 dropout=0.0, dataset=None, **kwargs):
+        super().__init__(**kwargs)
+        self.input_size=input_size
+        self.hidden_size=hidden_size
+        self.output_size=output_size
+        
+        self.num_layers=num_layers
+        self.rnn_dropout=dropout
+        
+        self.rnn = nn.LSTM(
+            self.input_size, self.hidden_size, num_layers=self.num_layers,
+            dropout=self.rnn_dropout, batch_first=True
+        ) ## batch_first=False !!
+        self.linear=nn.Linear(self.hidden_size, self.output_size)
+        
+        self.dataset=dataset
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=0.01)
+        self.loss_func = nn.MSELoss()
+    
+    def forward(self, x, h0=None, c0=None):
+        if h0 is None or c0 is None:
+            h0 = torch.zeros(self.num_layers, self.hidden_size).to(x.device)
+            c0 = torch.zeros(self.num_layers, self.hidden_size).to(x.device)
+        
+        # Forward pass through LSTM
+        output, (h1, c1) = self.rnn(x, (h0, c0))
+        #print(f"{output.shape = }")
+        output = self.linear(output[-1]) # Selecting the last output of LSTM
+        return (output, h1, c1)
+    
+    def train_epoch(self):
+        self.train(mode=True)
+        loss=0.0
+        for data in self.dataset:
+            loss+=self.train_sample(*data)
+        return loss/len(self.dataset)
+    
+    def train_sample(self, train_input, train_output):
+        h0, c0 = None, None # Initialize hidden and cell states
+        #print(f"{train_input.shape=}")
+        self.optimizer.zero_grad()
+        # Forward pass
+        outputs, h0, c0 = self(train_input.type(torch.float32), h0, c0)
+        
+        # Compute loss
+        loss = self.loss_func(outputs, train_output)
+        loss.backward()
+        self.optimizer.step()
+        
+        # Detach (copy) hidden and cell states
+        # to prevent backpropagation through the entire sequence
+        #h0 = h0.detach()
+        #c0 = c0.detach()
+        return loss
+    
+    def test_epoch(self):
+        return 0.0
 
 
 def main():
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    temp_model = TemperatureModel()
-    temp_model.to(device)
+    '''temp_model = TemperatureModel(100, 200, 2, 50, dropout=0.005, device=device)
+    temp_model.train_temp()'''
+    batch_size = 64
+    
+    captchas_set=CaptchasDataSet("data/captchas/train")
+    datasize=len(captchas_set)
+    #dataloader=DataLoader(captchas_set, batch_size=batch_size)
+    
+    temp_model=TemperatureModel(
+        50*50, 40*40, num_layers=2, output_size=6,
+        dataset=captchas_set
+    ).to(device)
+    
+    for epoch in range(1, 101):
+        loss=temp_model.train_epoch()
+        if epoch%10 == 0:
+            print(f"Epoch #{epoch}: loss={loss.item():.3f}")
+        #temp_model.test_epoch()
     
     return 0
 
