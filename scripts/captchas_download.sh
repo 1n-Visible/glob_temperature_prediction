1a074dfa06254445072f52415d8767aefa6218da
1nVisible <paskoarsen@gmail.com>
2025-01-04 18:10:16 +0200

    Added captcha download script

---
+++ scripts/captchas_download.sh
@@ -0,0 +1,4 @@
+curl -L -o ./data/captcha-image-dataset.zip \
+https://www.kaggle.com/api/v1/datasets/download/johnbergmann/captcha-image-dataset
+
+unzip -u ./data/captcha-image-dataset.zip -d data
